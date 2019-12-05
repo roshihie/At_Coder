2ed950d3e4c22b85042ceaa223c352451b767228
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2019-12-06 08:58:23 +0900

    2019/12/06 Commit

--- ABC_085_C_Otoshidama.cpp
+++ ABC_085_C_Otoshidama.cpp
@@ -1,66 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-struct StCoin
+void fnInput(int& rnTrgSu, int& rnTrgKin)
 {
-  StCoin() :
-    m_nCoin(0), m_nNum(0)
-  { }
-  StCoin(int nCoin, int nNum) :
-    m_nCoin(nCoin), m_nNum(nNum)
-  { }
-
-  int m_nCoin;
-  int m_nNum;
-};
-
-void fnInput(vector<StCoin>& rvoCoin, int& rnTrgKin)
-{
-  int nNum500, nNum100, nNum50;
-  cin >> nNum500 >> nNum100 >> nNum50;
-
-  int i = 0;
-  StCoin oCoin500(500, nNum500);
-  rvoCoin[i++] = oCoin500;
-  StCoin oCoin100(100, nNum100);
-  rvoCoin[i++] = oCoin100;
-  StCoin oCoin50(50, nNum50);
-  rvoCoin[i] = oCoin50;
-
-  cin >> rnTrgKin;
+  cin >> rnTrgSu >> rnTrgKin;
 }
 
-int fnRcsSch(int nPos, int nTrgKin, const vector<StCoin>& cnrvoCoin, int& rnSuccess)
+int fnRcsSch(int nPos, int nTrgSu, int nTrgKin, vector<int>& rvnNumMoney)
 {
-  if (!nTrgKin)
-  {
-    rnSuccess++;
-    return 1;
-  }
-  if (nTrgKin < 0)              return 1;
-  if (nPos == cnrvoCoin.size()) return 0;
+  vector<int> vnMoney{10000, 5000, 1000};
 
-  for (int i = 0; i <= cnrvoCoin[nPos].m_nNum; i++)
-    if (fnRcsSch(nPos + 1, nTrgKin - cnrvoCoin[nPos].m_nCoin * i, cnrvoCoin, rnSuccess))
-      break;
+  if (nTrgKin < 0)  return -1;
+  if (nPos == rvnNumMoney.size() - 1)
+  {
+    rvnNumMoney[nPos] = nTrgSu;
+    if (nTrgKin - vnMoney[nPos] * nTrgSu == 0)
+      return 1;
+    else
+      return 0;
+  }
+
+  for (int i = 0; i <= nTrgSu; i++)
+  {
+    rvnNumMoney[nPos] = i;
+    int nRtn = fnRcsSch(nPos + 1, nTrgSu - i, nTrgKin - vnMoney[nPos] * i, rvnNumMoney);
+    if      (nRtn == -1)  return 0;
+    else if (nRtn)        return 1;
+  }
 
   return 0;
 }
 
-void fnExhSch(int nTrgKin, const vector<StCoin>& cnrvoCoin)
+int fnExhSch(int nTrgSu, int nTrgKin)
 {
-  int nSuccess = 0;
-  fnRcsSch(0, nTrgKin, cnrvoCoin, nSuccess);
-  cout << nSuccess << endl;
+  vector<int> vnNumMoney(3);
+
+  if (fnRcsSch(0, nTrgSu, nTrgKin, vnNumMoney))
+    cout << vnNumMoney[0] << " " << vnNumMoney[1] << " " << vnNumMoney[2] << endl;
+  else
+    cout << -1 << " " << -1 << " " << -1 << endl;
 }
 
 int main()
 {
-  vector<StCoin> voCoin(3);
-  int nTrgKin;
-
-  fnInput(voCoin, nTrgKin);
-  fnExhSch(nTrgKin, voCoin);
+  int nTrgSu, nTrgKin;
+  
+  fnInput(nTrgSu, nTrgKin);
+  fnExhSch(nTrgSu, nTrgKin);
 
   return 0;
 }
