f9659d7967a6d7c8b93814b0a42f8977a8ef04cb
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-01-08 22:59:16 +0900

    2020/01/08 Commit

--- ABC057_C_Digits_in_Multiplicaton.cpp
+++ ABC057_C_Digits_in_Multiplicaton.cpp
@@ -23,9 +23,9 @@ int fnMultiplCheck(int64_t nNum)
   int nMinDigit = fnDigitsCheck(nNum);
                                            
   for (int64_t n = 2; n * n <= nNum; n++)  // int型 n では n * n が int型の範囲を
-  {                                        // 超えて桁落ちが発生し TLE となる
-    if (nNum % n != 0)  continue;          // nNum の平方根を超えない整数～nNum
-    int64_t nOtr = nNum / n;               // で左記検査を行うと TLE となる
+  {                                        // 超えて桁落ちが発生し 永久ループとなる
+    if (nNum % n != 0)  continue;          // nNum の平方根を超えない整数 + 1 ～ nNum
+    int64_t nOtr = nNum / n;               // で左記検査を行うと当然 TLE となる
     nMinDigit = min(nMinDigit, fnDigitsCheck(nOtr));
   }
   return nMinDigit;
