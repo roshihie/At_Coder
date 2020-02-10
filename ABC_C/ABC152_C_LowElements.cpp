a5ff9c32b4c15ff63773436e614462c4f5065d54
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-02-10 21:47:49 +0900

    2020/02/10 Commit

---
+++ ABC_C/ABC152_C_LowElements.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fnInput(vector<int>& rvnSeq)
+{
+  int nSeqSiz;
+  cin >> nSeqSiz;
+  rvnSeq.resize(nSeqSiz);
+
+  for(int& rnSeq : rvnSeq)
+    cin >> rnSeq;
+}
+  
+int fnLowElemCnt(const vector<int>& cnrvnSeq)
+{
+  int nLowElemCnt = 0;
+  int nMinLowElem = 200001;
+
+  for (int i = 0; i < cnrvnSeq.size(); i++)
+    if (cnrvnSeq[i] < nMinLowElem)
+    {
+      nLowElemCnt++;
+      nMinLowElem = cnrvnSeq[i];
+    }
+
+  return nLowElemCnt;
+}
+  
+int main()
+{
+  vector<int> vnSeq;
+
+  fnInput(vnSeq);
+  cout << fnLowElemCnt(vnSeq) << endl;
+
+  return 0;
+}
