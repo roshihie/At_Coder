aed678a76c5f7732ddca63692c421a4ed6ca24fa
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-02-17 23:02:39 +0900

    2020/02/17 Commit

---
+++ ABC_C/ABC138_C_Alchemist.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fnInput(vector<double>& rvnValue)
+{
+  int nValueSiz;
+  cin >> nValueSiz;
+  rvnValue.resize(nValueSiz);
+
+  for(double& rnValue : rvnValue)
+    cin >> rnValue;
+}
+  
+double fnMaxValueGet(const vector<double>& cnrvnValue)
+{
+  vector<double> vnAscValue(cnrvnValue);
+
+  while (vnAscValue.size() > 1)
+  {
+    sort(begin(vnAscValue), end(vnAscValue));
+    auto it = begin(vnAscValue);
+    double nAveValue = (double)( *it + *(it + 1) ) / 2;
+
+    vnAscValue.erase(it, it + 2);
+    vnAscValue.push_back(nAveValue);
+  }
+  return vnAscValue[0];
+}
+  
+int main()
+{
+  vector<double> vnValue;
+
+  fnInput(vnValue);
+  cout << fnMaxValueGet(vnValue) << endl;
+
+  return 0;
+}
