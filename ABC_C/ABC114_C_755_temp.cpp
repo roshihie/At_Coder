06277d67759f124e5c1721187c2824464414bdb4
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-05-05 12:53:55 +0900

    2020/05/05 Commit

--- ABC_C/ABC114_C_755_temp.cpp
+++ ABC_C/ABC114_C_755_temp.cpp
@@ -6,7 +6,7 @@ void fnInput(int& rnMaxNum)
   cin >> rnMaxNum;
 }
 
-int64_t fnToInt(const vector<char>& cnrvcNum)
+int64_t fnIntGet(const vector<char>& cnrvcNum)
 {
   const vector<int> cnvnDgt = {0, 3, 5, 7};
   int64_t nRtn = 0;
@@ -21,19 +21,21 @@ int64_t fnToInt(const vector<char>& cnrvcNum)
   return nRtn;
 }
 
-int fnDgt753_Check(const vector<char>& cnrvcNum)
+bool fnDgt753_Check(const vector<char>& cnrvcNum)
 {
-  int nDig3 = 0, nDig5 = 0, nDig7 = 0;
+  int nDgt3Cnt = 0;
+  int nDgt5Cnt = 0;
+  int nDgt7Cnt = 0;
 
-  for (int i = 0; i < cnrvcNum.size(); i++)
-    if      (cnrvcNum[i] == '1') nDig3++;
-    else if (cnrvcNum[i] == '2') nDig5++;
-    else if (cnrvcNum[i] == '3') nDig7++;
+  for (int i = cnrvcNum.size() - 1; i >= 0; i--)
+    if      (cnrvcNum[i] == '1') nDgt3Cnt++;
+    else if (cnrvcNum[i] == '2') nDgt5Cnt++;
+    else if (cnrvcNum[i] == '3') nDgt7Cnt++;
 
-  if (nDig3 && nDig5 && nDig7)
-    return 1;
-  else
-    return 0;
+  if (nDgt3Cnt && nDgt5Cnt && nDgt7Cnt)
+    return true;
+  else 
+    return false;
 }
 
 int fnDgt753_Count(int nMaxNum)
@@ -42,19 +44,17 @@ int fnDgt753_Count(int nMaxNum)
   int nDgtSiz = to_string(nMaxNum).size();
   vector<char> vcNum(nDgtSiz + 1, '0');
 
-  while (fnToInt(vcNum) <= nMaxNum) 
+  while (fnIntGet(vcNum) <= nMaxNum)
   {
     if (fnDgt753_Check(vcNum)) nDgt753Cnt++;
 
     vcNum[vcNum.size() - 1]++;
     for (int i = vcNum.size() - 1; i > 0; i--)
       if (vcNum[i] > '3')
-      {  
+      {
         vcNum[i - 1]++;
         vcNum[i] = '1';
       }
-      else
-        break;
   }
 
   return nDgt753Cnt;
