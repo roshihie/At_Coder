fba3db5e9c867c40d3973a1ead394cccc00d1431
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-05-10 17:29:25 +0900

    2020/05/10 Commit

--- ABC_C/ABC114_C_755_2.cpp
+++ ABC_C/ABC114_C_755_2.cpp
@@ -8,55 +8,51 @@ void fnInput(string& rsMaxNum)
 
 int fnDgtDP(string sMaxNum)
 {
-  // dp[i][j][k] :=  i: 上位 i 桁目まで決定
-  //                 j: small(数字を自由に選べる)か
-  //                 k: {3,5,7} を選んだかのflag
-  //                 の条件を満たす数値の数
+  // dp[nPos][nSmall][nFlag] :=  nPos: 上位 nPos 桁目まで決定
+  //                             nSmall: small(数字を自由に選べる)か
+  //                             nFlag: {3,5,7} を選んだかのflag
+  //                             の条件を満たす数値の数
   int dp[11][2][8] = {};
   dp[0][0][0] = 1;
 
   int nLen = sMaxNum.size();
 
-  for (int i = 0; i < nLen; ++i)
-  {
-    for (int j = 0; j < 2; ++j)
-    {
-      for (int k = 0; k < 8; ++k)
+  for (int nPos = 0; nPos < nLen; ++nPos)
+    for (int nSmall = 0; nSmall < 2; ++nSmall)
+      for (int nFlag = 0; nFlag < 8; ++nFlag)
       {
-        if (!dp[i][j][k]) continue;
+        if (!dp[nPos][nSmall][nFlag]) continue;
 
-        if (j == 1)                    // small ([0, 9] から数字を選べる)か
+        if (nSmall == 1)               // small ([0, 9] から数字を選べる)か
         {
-          if (!k) dp[i + 1][1][0] += dp[i][1][0];            // nothing
+          if (!nFlag) dp[nPos + 1][1][0] += dp[nPos][1][0];                // nothing
 
-          dp[i + 1][1][k | 1] += dp[i][1][k];                // +3
-          dp[i + 1][1][k | 2] += dp[i][1][k];                // +5
-          dp[i + 1][1][k | 4] += dp[i][1][k];                // +7
+          dp[nPos + 1][1][nFlag | 1] += dp[nPos][1][nFlag];                // +3
+          dp[nPos + 1][1][nFlag | 2] += dp[nPos][1][nFlag];                // +5
+          dp[nPos + 1][1][nFlag | 4] += dp[nPos][1][nFlag];                // +7
         }
         else                           // not small ([0, d] から数字を選べる)か
         {
-          int nDgt = sMaxNum[i] - '0';
+          int nDgt = sMaxNum[nPos] - '0';
 
-          if (!k)        dp[i + 1][1][0]     += dp[i][0][0]; // nothing
+          if (!nFlag)        dp[nPos + 1][1][0]     += dp[nPos][0][0];     // nothing
 
-          if (nDgt == 3) dp[i + 1][0][k | 1] += dp[i][0][k]; // +3
-          if (3 < nDgt)  dp[i + 1][1][k | 1] += dp[i][0][k]; // +3
-          if (nDgt == 5) dp[i + 1][0][k | 2] += dp[i][0][k]; // +5
-          if (5 < nDgt)  dp[i + 1][1][k | 2] += dp[i][0][k]; // +5
-          if (nDgt == 7) dp[i + 1][0][k | 4] += dp[i][0][k]; // +7
-          if (7 < nDgt)  dp[i + 1][1][k | 4] += dp[i][0][k]; // +7
+          if (nDgt == 3) dp[nPos + 1][0][nFlag | 1] += dp[nPos][0][nFlag]; // +3
+          if (3 < nDgt)  dp[nPos + 1][1][nFlag | 1] += dp[nPos][0][nFlag]; // +3
+          if (nDgt == 5) dp[nPos + 1][0][nFlag | 2] += dp[nPos][0][nFlag]; // +5
+          if (5 < nDgt)  dp[nPos + 1][1][nFlag | 2] += dp[nPos][0][nFlag]; // +5
+          if (nDgt == 7) dp[nPos + 1][0][nFlag | 4] += dp[nPos][0][nFlag]; // +7
+          if (7 < nDgt)  dp[nPos + 1][1][nFlag | 4] += dp[nPos][0][nFlag]; // +7
         }
       }
-    }
-  }
 /*
-  for (int i = 0; i <= nLen; ++i)
-     for (int j = 0; j < 2; ++j)
-       for (int k = 0; k < 8; ++k)
-         if (dp[i][j][k])
+  for (int nPos = 0; nPos <= nLen; ++nPos)
+     for (int nSmall = 0; nSmall < 2; ++nSmall)
+       for (int nFlag = 0; nFlag < 8; ++nFlag)
+         if (dp[nPos][nSmall][nFlag])
          {
-           cout << "dp[" << i << "][" << j << "][" << k << "] = (";
-           cout << dp[i][j][k] << ")" << endl;
+           cout << "dp[" << nPos << "][" << nSmall << "][" << nFlag << "] = (";
+           cout << dp[nPos][nSmall][nFlag] << ")" << endl;
          }
 */
   return dp[nLen][0][7] + dp[nLen][1][7];
