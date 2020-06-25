c89315b353974e0ccffc7fc23bfa0ab60adf80d3
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-06-25 22:14:40 +0900

    2020/06/25 Commit

---
+++ ABC_C/ABC127_C_Prison.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct StGate
+{
+  StGate() : 
+    m_nPassLow(0), m_nPassHigh(0) { }
+
+  int m_nPassLow;
+  int m_nPassHigh;
+};
+
+void fnInput(vector<StGate>& rvoGate)
+{
+  int  nPassSiz, nGateSiz;
+  cin >> nPassSiz >> nGateSiz;
+  rvoGate.resize(nGateSiz);
+
+  for (StGate& roGate : rvoGate)
+    cin >> roGate.m_nPassLow >> roGate.m_nPassHigh;
+}
+
+int fnAlmightyPass(const vector<StGate>& cnrvoGate)
+{
+  int nAlmightyPassLow = 0;
+  int nAlmightyPassHigh = 100001;
+
+  for (StGate oGate : cnrvoGate)
+  {
+    nAlmightyPassLow  = max(nAlmightyPassLow,  oGate.m_nPassLow);
+    nAlmightyPassHigh = min(nAlmightyPassHigh, oGate.m_nPassHigh);
+  }
+  return max(0, nAlmightyPassHigh - nAlmightyPassLow + 1);
+}
+  
+int main()
+{
+  vector<StGate> voGate;
+
+  fnInput(voGate);
+  cout << fnAlmightyPass(voGate) << endl;
+
+  return 0;
+}
