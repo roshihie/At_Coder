6936ef99f02982d3b56eda7c710231e853c0992b
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-06-28 22:24:31 +0900

    2020/06/28 Commit

---
+++ ABC_C/ABC172_C_Tsundoku.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+using llong = long long;
+
+void fnInput(int& rnLimit, vector<llong>& rvnCostA, vector<llong>& rvnCostB)
+{
+  int nCostASiz, nCostBSiz;
+  cin >> nCostASiz >> nCostBSiz >> rnLimit;
+  rvnCostA.resize(nCostASiz + 1);
+  rvnCostB.resize(nCostBSiz + 1);
+
+  for (int i = 1; i < rvnCostA.size(); i++)
+  {
+    int nOneCost;
+    cin >> nOneCost;
+    rvnCostA[i] = rvnCostA[i - 1] + nOneCost;
+  }
+
+  for (int i = 1; i < rvnCostB.size(); i++)
+  {
+    int nOneCost;
+    cin >> nOneCost;
+    rvnCostB[i] = rvnCostB[i - 1] + nOneCost;
+  }
+}
+
+int fnMaxSelCnt(int nLimit, const vector<llong>& cnrvnCostA, 
+                            const vector<llong>& cnrvnCostB)
+{
+  int nMaxSelCnt = 0;
+  int nBgnB = cnrvnCostB.size() - 1;
+  int nCntA = 0;
+
+  while (nCntA < cnrvnCostA.size()  &&
+         cnrvnCostA[nCntA] <= nLimit   )
+  {
+    int nCntB = nBgnB;
+
+    while (cnrvnCostB[nCntB] > nLimit - cnrvnCostA[nCntA])
+      nCntB--;
+
+    nMaxSelCnt = max(nMaxSelCnt, nCntA + nCntB);
+    nBgnB = nCntB;
+
+    nCntA++;
+  }
+  return nMaxSelCnt;
+}
+ 
+int main()
+{
+  int nLimit;
+  vector<llong> vnCostA, vnCostB;
+
+  fnInput(nLimit, vnCostA, vnCostB);
+  cout << fnMaxSelCnt(nLimit, vnCostA, vnCostB) << endl;
+
+  return 0;
+}
