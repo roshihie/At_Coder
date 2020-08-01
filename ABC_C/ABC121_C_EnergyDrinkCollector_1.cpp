182ab8f5085b133984798fcaa405b21627da1c51
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-08-01 21:38:48 +0900

    2020/08/01 Commit

---
+++ ABC_C/ABC121_C_EnergyDrinkCollector_1.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+using llong = long long;
+
+struct StGoods
+{
+  StGoods() : m_nPrice(0), m_nNum(0) { }
+
+  int m_nPrice;
+  int m_nNum;
+
+  bool operator<(const StGoods& rhs) const
+  {
+    return m_nPrice < rhs.m_nPrice;
+  }
+};
+
+void fnInput(int& rnTrgNum, vector<StGoods>& rvoGoods)
+{
+  int nGoodsSiz;
+  cin >> nGoodsSiz >> rnTrgNum;
+  rvoGoods.resize(nGoodsSiz);
+
+  for (StGoods& roGoods : rvoGoods)
+    cin >> roGoods.m_nPrice >> roGoods.m_nNum;
+}
+
+llong fnMinCost(int nTrgNum, vector<StGoods>& rvoGoods)
+{
+  sort(begin(rvoGoods), end(rvoGoods));
+
+  llong nCost = 0;
+
+  for (StGoods oGoods : rvoGoods)
+  {
+    int nUsedNum = min(nTrgNum, oGoods.m_nNum);
+    nCost += (llong)oGoods.m_nPrice * nUsedNum;
+    nTrgNum -= nUsedNum;
+
+    if ( !nTrgNum ) break;
+  }
+  return nCost;
+}
+ 
+int main()
+{
+  int nTrgNum;
+  vector<StGoods> voGoods;
+
+  fnInput(nTrgNum, voGoods);
+  cout << fnMinCost(nTrgNum, voGoods) << endl;
+
+  return 0;
+}
