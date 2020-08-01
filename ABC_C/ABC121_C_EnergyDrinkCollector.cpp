b1287629296c9a1d17b8d07a9f9c2c03d36be6e4
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-08-01 20:37:40 +0900

    2020/8/1 Commit

---
+++ ABC_C/ABC121_C_EnergyDrinkCollector.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+using llong = long long;
+
+struct StGoods
+{
+  StGoods() : m_nPrice(0), m_nNum(0) { }
+
+  int m_nPrice;
+  int m_nNum;
+};
+
+void fnInput(int& rnTrgNum, vector<StGoods>& rvoGoods)
+{
+  int nGoodsSiz;
+  cin >> nGoodsSiz >> rnTrgNum;
+  rvoGoods.resize(nGoodsSiz);
+
+  for (StGoods& roGoods : rvoGoods)
+    cin >> roGoods.m_nPrice >> roGoods.m_nNum;
+}
+
+llong fnMinCost(int nTrgNum, const vector<StGoods>& cnrvoGoods)
+{
+  map<int, int> mpGoods;
+
+  for (StGoods oGoods : cnrvoGoods)
+    mpGoods[ oGoods.m_nPrice ] += oGoods.m_nNum;
+
+  llong nCost = 0;
+
+  for (auto PGoods : mpGoods)
+  {
+    int nUsedNum = min(nTrgNum, PGoods.second);
+    nCost += (llong)PGoods.first * nUsedNum;
+    nTrgNum -= nUsedNum;
+
+    if ( !nTrgNum ) break;
+  }
+  return nCost;
+}
+ 
+int main()
+{
+  int nTrgNum;
+  vector<StGoods> voGoods;
+
+  fnInput(nTrgNum, voGoods);
+  cout << fnMinCost(nTrgNum, voGoods) << endl;
+
+  return 0;
+}
--- ABC_C/ABC122_C_Get_AC.cpp
+++ ABC_C/ABC122_C_Get_AC.cpp
@@ -27,7 +27,7 @@ void fnSubStrACCnt(string sStr, const vector<StQuery>& cnrvoQuery)
 
   //vnCumlSum_ix 0 1 2 3 4 5 6 7 8
   //sStr_pos      1 2 3 4 5 6 7 8
-  //sStr_pos      A C A C T A C G
+  //sStr          A C A C T A C G
   //sStr_ix       0 1 2 3 4 5 6 7
   for (int nBgn = 0; nBgn < vnCumlSum.size() - 1; nBgn++)
   {
