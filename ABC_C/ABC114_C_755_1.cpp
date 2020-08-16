862e41bebb3b1741a4e9cce3bbb1dd104d2dfb6f
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-08-16 18:10:11 +0900

    2020/08/16 Commit

--- ABC_C/ABC114_C_755_1.cpp
+++ ABC_C/ABC114_C_755_1.cpp
@@ -8,9 +8,11 @@ void fnInput(string& rsMaxNum)
 
 int fnDgtDP(string sMaxNum)
 {
-  // dp[nPos][nSmall][nFlag] :=  nPos: 上位 nPos 桁目まで決定
+  // dp[nPos][nSmall][nFlag] :=  nPos  : 上位 nPos 桁目まで決定
   //                             nSmall: small(数字を自由に選べる)か
-  //                             nFlag: {3,5,7} を選んだかのflag
+  //                             nFlag : 上位 nPos 桁までで {3,5,7} を選択したかflag
+  //                             ( 0:選択なし, 1: 3   選択, 2: 5   選択, 3: 3 5   選択, 
+  //                               4: 7 選択 , 5: 3 7 選択, 6: 5 7 選択, 7: 3 5 7 選択 )
   //                             の条件を満たす数値の数
   int dp[11][2][8] = {};
   dp[0][0][0] = 1;
@@ -25,24 +27,25 @@ int fnDgtDP(string sMaxNum)
 
         if (nSmall == 1)               // small ([0, 9] から数字を選べる)か
         {
-          if (!nFlag) dp[nPos + 1][1][0] += dp[nPos][1][0];                // nothing
+          if (!nFlag)
+            dp[nPos + 1][1][0]       += dp[nPos][1][0];                    // nothing
 
           dp[nPos + 1][1][nFlag | 1] += dp[nPos][1][nFlag];                // +3
           dp[nPos + 1][1][nFlag | 2] += dp[nPos][1][nFlag];                // +5
           dp[nPos + 1][1][nFlag | 4] += dp[nPos][1][nFlag];                // +7
         }
-        else                           // not small ([0, d] から数字を選べる)か
+        else                           // not small ([0, nDgt] から数字を選べる)か
         {
           int nDgt = sMaxNum[nPos] - '0';
 
-          if (!nFlag)        dp[nPos + 1][1][0]     += dp[nPos][0][0];     // nothing
+          if (!nFlag)    dp[nPos + 1][1][0]         += dp[nPos][0][0];     // nothing
 
           if (nDgt == 3) dp[nPos + 1][0][nFlag | 1] += dp[nPos][0][nFlag]; // +3
-          if (3 < nDgt)  dp[nPos + 1][1][nFlag | 1] += dp[nPos][0][nFlag]; // +3
+          if (3 < nDgt ) dp[nPos + 1][1][nFlag | 1] += dp[nPos][0][nFlag]; // +3
           if (nDgt == 5) dp[nPos + 1][0][nFlag | 2] += dp[nPos][0][nFlag]; // +5
-          if (5 < nDgt)  dp[nPos + 1][1][nFlag | 2] += dp[nPos][0][nFlag]; // +5
+          if (5 < nDgt ) dp[nPos + 1][1][nFlag | 2] += dp[nPos][0][nFlag]; // +5
           if (nDgt == 7) dp[nPos + 1][0][nFlag | 4] += dp[nPos][0][nFlag]; // +7
-          if (7 < nDgt)  dp[nPos + 1][1][nFlag | 4] += dp[nPos][0][nFlag]; // +7
+          if (7 < nDgt ) dp[nPos + 1][1][nFlag | 4] += dp[nPos][0][nFlag]; // +7
         }
       }
 /*
