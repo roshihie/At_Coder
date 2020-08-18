722d8a9ffdc6746ed2f1448fe7911b630e28c966
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-08-19 07:57:00 +0900

    2020/08/19 Commit

---
+++ ABC_C/ABC175_C_WalkingTakahashi.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+using llong = long long;
+
+void fnInput(llong& rnCoordx, llong& rnTimes, llong& rnMovmnt)
+{
+  cin >> rnCoordx >> rnTimes >> rnMovmnt;
+}
+  
+llong fnMinCoordx(llong nCoordx, llong nTimes, llong nMovmnt)
+{
+  llong nQuotent  = abs(nCoordx) / nMovmnt;
+  llong nRemain   = abs(nCoordx) % nMovmnt;
+
+  if (nQuotent <= nTimes)
+    if (nQuotent % 2 == nTimes % 2)
+      return nRemain;
+    else
+      return abs(nRemain - nMovmnt);
+  else
+    return abs(nCoordx) - nTimes * nMovmnt;
+}
+  
+int main()
+{
+  llong nCoordx, nTimes, nMovmnt;
+
+  fnInput(nCoordx, nTimes, nMovmnt);
+  cout << fnMinCoordx(nCoordx, nTimes, nMovmnt) << endl;
+
+  return 0;
+}
