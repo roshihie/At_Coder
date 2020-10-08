72f8148ae3f59f7ec2ca38aab69b8847f2a6218e
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-10-09 07:53:37 +0900

    2020/10/09 Commit

--- ABC_C/ABC113_C_ID.cpp
+++ ABC_C/ABC113_C_ID.cpp
@@ -26,25 +26,25 @@ void calcIDNum(int nPrefSiz, const vector<StCity>& cnrvoCity)
 {
   vector<vector<int>> vvnCity(nPrefSiz);
                                            // vvnCity[ nPref ] に nYear を保存 
-  for (int i = 0; i < cnrvoCity.size(); ++i)
-    vvnCity[ cnrvoCity[i].m_nPref ].push_back( cnrvoCity[i].m_nYear );
+  for (int nx = 0; nx < cnrvoCity.size(); ++nx)
+    vvnCity[ cnrvoCity[nx].m_nPref ].push_back( cnrvoCity[nx].m_nYear );
 
-  for (int i = 0; i < vvnCity.size(); ++i)
+  for (int nx = 0; nx < vvnCity.size(); ++nx)
   {                                        // vvnCity[ nPref ] 毎に nYear を昇順に sort
-    sort( begin(vvnCity[i]), end(vvnCity[i]) );
+    sort( begin(vvnCity[nx]), end(vvnCity[nx]) );
                                            // vvnCity[ nPref ] 毎に nYear の重複を除く
-    vvnCity[i].erase( unique( begin(vvnCity[i]), end(vvnCity[i]) ), end(vvnCity[i]) );
+    vvnCity[nx].erase( unique( begin(vvnCity[nx]), end(vvnCity[nx]) ), end(vvnCity[nx]) );
   }
 
-  for (int i = 0; i < cnrvoCity.size(); ++i)
+  for (int nx = 0; nx < cnrvoCity.size(); ++nx)
   {
-    int nPref = cnrvoCity[i].m_nPref;
+    int nPref = cnrvoCity[nx].m_nPref;
     cout << setfill('0') << setw(6) << nPref + 1;
                    // nYear で sort済の vvnCity[ nPref ] に対して、求める nYear を二分索引して
                    // イテレータを算出し、vvnCity[ nPref ]の begin イテレータを削除し、
                    // nYear が小さい方から何番目かを取得する
     int nIdNum = lower_bound( begin( vvnCity[ nPref ] ), end( vvnCity[ nPref ] ), 
-                              cnrvoCity[i].m_nYear ) - begin( vvnCity[ nPref ] ); 
+                              cnrvoCity[nx].m_nYear ) - begin( vvnCity[ nPref ] ); 
     cout << setfill('0') << setw(6) << nIdNum + 1 << endl;
   }
 }
