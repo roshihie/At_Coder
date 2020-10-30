f943d89008523769562157310972972eb82c6819
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-10-30 21:03:50 +0900

    2020/10/30 Commit

--- ABC_C/ABC155_C_Poll.cpp
+++ ABC_C/ABC155_C_Poll.cpp
@@ -6,11 +6,11 @@ void input(map<string, int>& rmpPoll)
   int nPollSiz;
   cin >> nPollSiz;
 
-  for (int i = 0; i <= nPollSiz; i++)
+  for (int nx = 0; nx < nPollSiz; ++nx)
   {
     string sPoll;
     cin >> sPoll;
-    rmpPoll[sPoll]++;
+    ++rmpPoll[sPoll];
   }
 }
   
@@ -22,10 +22,8 @@ void calcMaxPoll(const map<string, int>& cnrmpPoll)
     nMaxCnt = max(nMaxCnt, PPoll.second);
 
   for (pair<string, int> PPoll : cnrmpPoll)
-  {
     if (PPoll.second == nMaxCnt)
       cout << PPoll.first << endl;
-  }
 }
   
 int main()
