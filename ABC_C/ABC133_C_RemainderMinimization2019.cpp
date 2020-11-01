7c8c6f595b4df2a04841fc720ea1195cc350f0b7
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2020-11-01 19:15:37 +0900

    2020/11/01 Commit

--- ABC_C/ABC133_C_RemainderMinimization2019.cpp
+++ ABC_C/ABC133_C_RemainderMinimization2019.cpp
@@ -13,10 +13,10 @@ int calcMinRemain2019(int nLeft, int nRigt)
                                                // 2019 で割り切れる数が存在
   int nMinRem2019 = 2018;
 
-  for (int i = nLeft; i < nRigt; i++)
-    for (int j = i + 1; j < nRigt + 1; j++)
+  for (int nx = nLeft; nx < nRigt; ++nx)
+    for (int ny = nx + 1; ny < nRigt + 1; ++ny)
     {
-      int nRem2019 = (llong)i * j % 2019;
+      int nRem2019 = (llong)nx * ny % 2019;
       nMinRem2019 = min(nMinRem2019, nRem2019);
     }
 
