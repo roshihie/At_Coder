0cfd49c4b17751d30fdad2c39ed7e62589644076
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2021-04-04 09:00:22 +0900

    2021/04/04 Commit

--- ABC_C/ABC147_C_Honest_or_Unkind2.cpp
+++ ABC_C/ABC147_C_Honest_or_Unkind2.cpp
@@ -15,34 +15,35 @@ void input(vector<vector<StExpr>>& rvvoExpr)
   cin >> nPersonSiz;
   rvvoExpr.resize(nPersonSiz);
 
-  for (int nx = 0; nx < rvvoExpr.size(); ++nx)
+  for (int ny = 0; ny < rvvoExpr.size(); ++ny)
   {
     int nAttrSiz;
     cin >> nAttrSiz;
-    rvvoExpr[nx].resize(nAttrSiz);
+    rvvoExpr[ny].resize(nAttrSiz);
 
-    for (int ny = 0; ny < rvvoExpr[nx].size(); ++ny)
-      cin >> rvvoExpr[nx][ny].m_nPerson >> rvvoExpr[nx][ny].m_nAttr;
+    for (int nx = 0; nx < rvvoExpr[ny].size(); ++nx)
+      cin >> rvvoExpr[ny][nx].m_nPerson >> rvvoExpr[ny][nx].m_nAttr;
   }
 }
  
 int isHonest(const vector<vector<StExpr>>& cnrvvoExpr, 
-                  const vector<int>& cnrvnHonest, int nMan)
+             const vector<int>& cnrvnHonest, int nMan)
 {
   if (cnrvnHonest[nMan])               // Honest
   {
-    for (int ny = 0; ny < cnrvvoExpr[nMan].size(); ++ny)
-      if (cnrvvoExpr[nMan][ny].m_nAttr  !=
-          cnrvnHonest[ cnrvvoExpr[nMan][ny].m_nPerson - 1 ] )
+    for (int nx = 0; nx < cnrvvoExpr[nMan].size(); ++nx)
+      if (cnrvvoExpr[nMan][nx].m_nAttr  ==
+          cnrvnHonest[ cnrvvoExpr[nMan][nx].m_nPerson - 1 ] );
+      else
         return 0;
 
     return 1;
   }
   else                                 // UnKind
   {
-    for (int ny = 0; ny < cnrvvoExpr[nMan].size(); ++ny)
-      if (cnrvvoExpr[nMan][ny].m_nAttr  !=
-          cnrvnHonest[ cnrvvoExpr[nMan][ny].m_nPerson - 1 ] )
+    for (int nx = 0; nx < cnrvvoExpr[nMan].size(); ++nx)
+      if (cnrvvoExpr[nMan][nx].m_nAttr  !=
+          cnrvnHonest[ cnrvvoExpr[nMan][nx].m_nPerson - 1 ] );
         return 1;
 
     return 0;
@@ -54,29 +55,29 @@ int calcMaxHonest(const vector<vector<StExpr>>& cnrvvoExpr)
   vector<int> vnHonest(cnrvvoExpr.size());
   int nMaxHonestCnt = 0;
   
-  for (int nBit = 0; nBit < ( 1 << cnrvvoExpr.size() ); ++nBit)
+  for (int nBit = 0; nBit < ( 1 << vnHonest.size() ); ++nBit)
   {
     int nHonestCnt = 0;
 
-    for (int nEach = 0; nEach < cnrvvoExpr.size() ; ++nEach)
+    for (int nEach = 0; nEach < vnHonest.size() ; ++nEach)
       if (nBit & ( 1 << nEach ))
       {
-        vnHonest[nEach] = 1;            // Honest
+        vnHonest[nEach] = 1;           // Honest
         ++nHonestCnt;
       }
       else
-        vnHonest[nEach] = 0;            // UnKind
+        vnHonest[nEach] = 0;           // UnKind
 
-    bool bReslt = true;
+    bool bRslt = true;
 
     for (int nMan = 0; nMan < vnHonest.size(); ++nMan)
-      if (!isHonest(cnrvvoExpr, vnHonest, nMan))
+      if ( !isHonest(cnrvvoExpr, vnHonest, nMan) )
       {
-        bReslt = false;
+        bRslt = false;
         break;
       }
 
-    if (bReslt) nMaxHonestCnt = max(nMaxHonestCnt, nHonestCnt);
+    if (bRslt) nMaxHonestCnt = max(nMaxHonestCnt, nHonestCnt);
   }
   return nMaxHonestCnt;
 }
