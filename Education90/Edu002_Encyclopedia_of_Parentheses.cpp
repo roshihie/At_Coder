bf3d8b77abe0a78bcf1e1cd1b0dc06deac3a654d
Hiroshi Kasahara <roshihie@zb.wakwak.com>
2021-11-30 21:10:01 +0900

    2021/11/30 Commit

---
+++ Education90/Edu002_Encyclopedia_of_Parentheses.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void input(int& rnNum)
+{
+  cin >> rnNum;
+}
+
+void outPairKakko(int nNum, int nBit)
+{
+  for (int nEach = nNum - 1; nEach >= 0; --nEach)
+    if (nBit & ( 1 << nEach ))
+      cout << ")";
+    else
+      cout << "(";
+
+  cout << endl;
+}
+
+void chkPairKakko(int nNum)
+{
+  for (int nBit = 0; nBit < ( 1 << nNum ); ++nBit)
+  {
+    int nLeftkkCnt = 0, nRigtkkCnt = 0;
+    bool bPair = true;
+
+    for (int nEach = nNum - 1; nEach >= 0; --nEach)
+    {
+      if (nBit & ( 1 << nEach ))
+         ++nRigtkkCnt;
+      else
+         ++nLeftkkCnt;
+
+      if (nLeftkkCnt < nRigtkkCnt)
+      {
+        bPair = false;
+        break;
+      }
+    }
+    if (bPair && nLeftkkCnt == nRigtkkCnt)
+       outPairKakko(nNum, nBit);
+  }
+}
+  
+int main()
+{
+  int nNum;
+  input(nNum);
+  chkPairKakko(nNum);
+
+  return 0;
+}
